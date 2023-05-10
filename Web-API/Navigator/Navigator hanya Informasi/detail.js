a0003c3a62715898ca7455ee67c05c173159b540
Muhammad Dava Fahreza <mdavafahreza05@gmail.com>
2023-05-10 14:01:01 +0700

    video tentang property yang tidak memiliki prototype dan video sejarah web api

---
+++ Web-API/Navigator/Navigator hanya Informasi/detail.js
@@ -0,0 +1,159 @@
+// 1.appCodeName: Menunjukkan kode nama dari aplikasi browser yang sedang digunakan. Pada contoh di atas, nilai dari appCodeName adalah "Mozilla".
+console.log(navigator.appCodeName)
+
+// 2.appName: Menunjukkan nama dari aplikasi browser yang sedang digunakan. Pada contoh di atas, nilai dari appName adalah "Netscape".
+console.log(navigator.appName)
+
+// 3.appVersion: Menunjukkan versi dari aplikasi browser yang sedang digunakan beserta informasi sistem operasi. Pada contoh di atas, nilai dari appVersion adalah "5.0 
+// (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36".
+console.log(navigator.appVersion)
+
+// 4.bluetooth: Menyediakan informasi dan interaksi dengan perangkat Bluetooth yang terhubung pada perangkat yang digunakan.
+console.log(navigator.bluetooth)
+
+// 5.clipboard: Menyediakan interaksi dengan clipboard atau papan klip perangkat yang digunakan.
+console.log(navigator.clipboard)
+
+// 6.connection: Menyediakan informasi tentang koneksi jaringan, seperti kecepatan internet, jenis koneksi, dan sebagainya.
+console.log(navigator.connection)
+
+// 7.cookieEnabled: Menunjukkan apakah pengguna mengizinkan penggunaan cookie atau tidak.
+console.log(navigator.cookieEnabled)
+
+// 8.credentials: Menyediakan interaksi dengan kredensial atau informasi login pengguna yang disimpan pada browser.
+console.log(navigator.credentials)
+
+// 9.deviceMemory: Menunjukkan kapasitas memori perangkat pengguna dalam gigabyte.
+console.log(navigator.deviceMemory)
+
+// 10.doNotTrack: Menunjukkan apakah pengguna telah mengaktifkan fitur "Do Not Track" pada browser atau tidak.
+console.log(navigator.doNotTrack)
+
+// 11.geolocation: Menyediakan interaksi dengan layanan geolokasi yang digunakan untuk mendapatkan lokasi pengguna.
+console.log(navigator.geolocation)
+
+// 12.hardwareConcurrency: Menunjukkan jumlah core prosesor pada perangkat yang digunakan.
+console.log(navigator.hardwareConcurrency)
+
+// 13.hid: Menyediakan interaksi dengan perangkat HID (Human Interface Device) seperti mouse dan keyboard.
+console.log(navigator.hid)
+
+// 14.ink: Menyediakan interaksi dengan perangkat pengguna yang mendukung teknologi pen seperti stylus dan sejenisnya.
+console.log(navigator.ink)
+
+// 15.keyboard: Menyediakan interaksi dengan keyboard yang terhubung pada perangkat yang digunakan.
+console.log(navigator.keyboard)
+
+// 16.language: Menunjukkan bahasa pengguna yang digunakan pada browser.
+console.log(navigator.language)
+
+// 17.languages: Menunjukkan daftar bahasa yang didukung oleh browser.
+console.log(navigator.languages)
+
+// 18.locks: Menyediakan fitur penguncian untuk menghindari interaksi secara tidak sengaja pada perangkat yang digunakan.
+console.log(navigator.locks)
+
+// 19.managed: Menyediakan informasi mengenai konfigurasi perangkat yang diatur oleh administrator.
+console.log(navigator.managed)
+
+// 20.maxTouchPoints: Menunjukkan jumlah maksimum titik sentuhan yang didukung oleh perangkat yang digunakan.
+console.log(navigator.maxTouchPoints)
+
+// 21.mediaCapabilities: Menyediakan informasi mengenai kemampuan media perangkat pengguna seperti video dan audio.
+console.log(navigator.mediaCapabilities)
+
+// 22.mediaDevices: Menyediakan interaksi dengan perangkat media seperti kamera dan mikrofon yang terhubung pada perangkat yang digunakan.
+console.log(navigator.mediaDevices)
+
+// 23.mediaSession: Menyediakan interaksi dengan media session yang sedang berjalan pada perangkat yang digunakan.
+console.log(navigator.mediaSession)
+
+// 24.mimeTypes: Menyediakan daftar MIME type yang didukung oleh browser.
+console.log(navigator.mimeTypes)
+
+// 25.onLine: Menunjukkan apakah perangkat yang digunakan sedang terhubung ke internet atau tidak.
+console.log(navigator.onLine)
+
+// 26.pdfViewerEnabled: Menunjukkan apakah browser mendukung fitur PDF viewer atau tidak.
+console.log(navigator.pdfViewerEnabled)
+
+// 27.permissions: Menyediakan interaksi dengan izin akses yang
+console.log(navigator.permissions)
+
+// 28.mimeTypes: Berisi daftar objek MimeType yang merepresentasikan tipe MIME yang didukung oleh browser.
+console.log(navigator.mimeTypes)
+
+// 29.onLine: Menunjukkan apakah koneksi internet sedang aktif atau tidak.
+console.log(navigator.onLine)
+
+// 30.pdfViewerEnabled: Menunjukkan apakah PDF viewer bawaan browser diaktifkan atau tidak.
+console.log(navigator.pdfViewerEnabled)
+
+// 31.permissions: Berisi objek Permissions yang menyediakan API untuk meminta izin kepada pengguna.
+console.log(navigator.permissions)
+
+// 32.platform: Menunjukkan platform sistem operasi yang digunakan oleh user, dalam hal ini adalah "Win32" atau Windows 32-bit.
+console.log(navigator.platform)
+
+// 33.plugins: Berisi daftar objek Plugin yang merepresentasikan plugin yang tersedia di browser.
+console.log(navigator.plugins)
+
+// 34.presentation: Berisi objek Presentation yang menyediakan API untuk mengatur presentasi layar.
+console.log(navigator.presentation)
+
+// 35.product: Menunjukkan nama produk browser, dalam hal ini adalah "Gecko".
+console.log(navigator.product)
+
+// 36.productSub: Menunjukkan subversi produk browser.
+console.log(navigator.productSub)
+
+// 37.scheduling: Berisi objek Scheduling yang menyediakan API untuk menjadwalkan tugas yang akan dijalankan oleh browser.
+console.log(navigator.scheduling)
+
+// 38.serial: Berisi objek Serial yang menyediakan API untuk berkomunikasi dengan perangkat serial.
+console.log(navigator.serial)
+
+// 39.serviceWorker: Berisi objek ServiceWorkerContainer yang menyediakan API untuk mengatur service worker.
+console.log(navigator.serviceWorker)
+
+// 40.storage: Berisi objek StorageManager yang menyediakan API untuk mengatur penyimpanan data pada browser.
+console.log(navigator.storage)
+
+// 41.usb: Berisi objek USB yang menyediakan API untuk berkomunikasi dengan perangkat USB.
+console.log(navigator.usb)
+
+// 42.userActivation: Berisi objek UserActivation yang menyediakan informasi tentang aktivasi pengguna dalam browser.
+console.log(navigator.userActivation)
+
+// 43.userAgent: Menunjukkan informasi tentang user agent browser.
+console.log(navigator.userAgent)
+
+// 44.userAgentData: Berisi objek NavigatorUAData yang menyediakan informasi lebih detail tentang user agent browser.
+console.log(navigator.userAgentData)
+
+// 45.vendor: Menunjukkan vendor browser, dalam hal ini adalah "Google Inc.".
+console.log(navigator.vendor)
+
+// 46.vendorSub: Menunjukkan subversi vendor browser.
+console.log(navigator.vendorSub)
+
+// 47.virtualKeyboard: Berisi objek VirtualKeyboard yang menyediakan API untuk menampilkan virtual keyboard pada perangkat touchscreen.
+console.log(navigator.virtualKeyboard)
+
+// 48.wakeLock: Berisi objek WakeLock yang menyediakan API untuk mengatur wake lock pada perangkat.
+console.log(navigator.wakeLock)
+
+// 49.webdriver: Menunjukkan apakah browser dijalankan dalam mode WebDriver atau tidak.
+console.log(navigator.webdriver)
+
+// 50.webkitPersistentStorage: Berisi objek DeprecatedStorageQuota yang menyediakan API untuk mengatur penyimpanan data persisten pada browser.
+console.log(navigator.webkitPersistentStorage)
+
+// 51.webkitTemporaryStorage: Berisi objek DeprecatedStorageQuota yang menyediakan API untuk mengatur penyimpanan data sementara pada browser.
+console.log(navigator.webkitTemporaryStorage)
+
+// 52.windowControlsOverlay: Berisi objek WindowControlsOverlay yang menyediakan API untuk mengatur overlay kontrol jendela pada browser.
+console.log(navigator.windowControlsOverlay)
+
+// 53.xr: Berisi objek XRSystem yang menyediakan API untuk berinteraksi dengan Virtual Reality (VR) dan Augmented Reality (AR) pada browser.
+console.log(navigator.xr)
---
+++ Web-API/Navigator/Navigator hanya Informasi/index.js
@@ -0,0 +1,53 @@
+// 1 navigator.appCodeName: Menunjukkan kode nama dari aplikasi browser yang sedang digunakan. Pada contoh di atas, nilai dari appCodeName adalah "Mozilla".
+console.log(navigator.appCodeName)
+
+// 2 navigator.appName: Menunjukkan nama dari aplikasi browser yang sedang digunakan. Pada contoh di atas, nilai dari appName adalah "Netscape".
+console.log(navigator.appName)
+
+// 3 navigator.appVersion: Menunjukkan versi dari aplikasi browser yang sedang digunakan beserta informasi sistem operasi. Pada contoh di atas, nilai dari appVersion adalah "5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36".
+console.log(navigator.appVersion)
+
+// 7 navigator.cookieEnabled: Menunjukkan apakah pengguna mengizinkan penggunaan cookie atau tidak.
+console.log(navigator.cookieEnabled)
+
+// 9 navigator.deviceMemory: Menunjukkan kapasitas memori perangkat pengguna dalam gigabyte.
+console.log(navigator.deviceMemory)
+
+// 10 navigator.doNotTrack: Menunjukkan apakah pengguna telah mengaktifkan fitur "Do Not Track" pada browser atau tidak.
+console.log(navigator.doNotTrack)
+
+// 12 navigator.hardwareConcurrency: Menunjukkan jumlah core prosesor pada perangkat yang digunakan.
+console.log(navigator.hardwareConcurrency)
+
+// 16 navigator.language: Menunjukkan bahasa pengguna yang digunakan pada browser.
+console.log(navigator.language)
+
+// 17 navigator. languages: Menunjukkan daftar bahasa yang didukung oleh browser.
+console.log(navigator.languages)
+
+// 20 navigator.maxTouchPoints: Menunjukkan jumlah maksimum titik sentuhan yang didukung oleh perangkat yang digunakan.
+console.log(navigator.maxTouchPoints)
+
+// 25 navigator. onLine: Menunjukkan apakah perangkat yang digunakan sedang terhubung ke internet atau tidak.
+console.log(navigator.onLine)
+
+// 26 navigator.pdfViewerEnabled: Menunjukkan apakah browser mendukung fitur PDF viewer atau tidak.
+console.log(navigator.pdfViewerEnabled)
+
+// 32 navigator.platform: Menunjukkan platform sistem operasi yang digunakan oleh user, dalam hal ini adalah "Win32" atau Windows 32-bit.
+console.log(navigator.platform)
+
+// 35 navigator.product: Menunjukkan nama produk browser, dalam hal ini adalah "Gecko".
+console.log(navigator.product)
+
+// 36 navigator.productSub: Menunjukkan subversi produk browser.
+console.log(navigator.productSub)
+
+// 45 navigator.vendor: Menunjukkan vendor browser, dalam hal ini adalah "Google Inc.".
+console.log(navigator.vendor)
+
+// 46 navigator.vendorSub: Menunjukkan subversi vendor browser.
+console.log(navigator.vendorSub)
+
+// 49 navigator.webdriver: Menunjukkan apakah browser dijalankan dalam mode WebDriver atau tidak.
+console.log(navigator.webdriver)
