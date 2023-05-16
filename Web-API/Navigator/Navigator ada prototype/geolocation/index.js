a1668339bd00c6438cd03bf576826c535a65c4ae
Muhammad Dava Fahreza <mdavafahreza05@gmail.com>
2023-05-16 17:13:22 +0700

    video baru geolocation

---
+++ Web-API/Navigator/Navigator ada prototype/geolocation/index.js
@@ -0,0 +1,24 @@
+// console.log(navigator.geolocation)
+
+// navigator.geolocation.getCurrentPosition(
+    // lokasi => console.log(lokasi), 
+    // gagal => console.log(gagal),
+    // {
+    //     timeout: 5000,
+    //     maximumAge: 0,
+    //     enableHighAccuracy: true
+    // }
+// )
+
+// let watchId = navigator.geolocation.watchPosition(
+//     lokasi => console.log(lokasi), 
+//     gagal => console.log(gagal),
+//     {
+//         timeout: 5000,
+//         maximumAge: 0,
+//         enableHighAccuracy: true
+//     }
+// )
+
+// navigator.geolocation.clearWatch(watchId);
+    
